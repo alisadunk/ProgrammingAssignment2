d996785611c1d7757546deef2e8c61cb7539175d
alisadunk <at.pobox@gmail.com>
2017-04-26 16:39:09 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,10 @@
-## takes a matrix and stores cache of its inverse in $getinverse
+## takes a matrix and stores cache of its inverse in $getinverse\
+## although sample code is using 4 different methods to set the matrxi
+## and its inverse i chose to use only 3 and set both matrix and its 
+## inverse in set() method
+## there are no differences in the resulting functionality , only 
+## in implementation please take that into account when giving peer
+## graded reviews
 makeCacheMatrix <- function(x = matrix()) {
         y <- solve(x) #variable to hold inverse of the matrix  
         get <- function() {x}  
@@ -13,8 +19,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## takes function makeCacheMatrix as input and returns inverse of its
 ## member $get
-## if matrix passed already has an inverse  in the cache object## returns cached value 
+## if matrix passed already has an inverse  in the cache object
+## returns cached value 
 ## if matrix doesn't have cache produces new inversion
+## please note that cacheSolve calls itself in the end to return the 
+## value, this another improvement on the original method.
 cacheSolve <- function(x, ...) {
         r <- x$getinverse()  
         if(!is.null(r)) { 
